24457a3c3eaee47ba2e1a746b9ed3ffda5826d49
Bousrahu-umuhoza <bousrahumuhoza@gmail.com>
2023-01-06 13:00:50 +0200

    input

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,22 +1,53 @@
- type Props = {
-    type: "text" | "email" | "password" | "number",
-    label: "string",
-    helperText: "text",
-    error: "text"
-    onchange: (evt: Event)=>void
 
+import "./style.css"
+import { ChangeEventHandler, useState } from "react";
+
+type Props = {
+    name: string, 
+    type?: "email" | "password" | "text" | "number" | "date" | "Username", 
+    variant: "one" | "two" | "three", 
+    label: string,
+    helperText?: string,
+    errorText?: string,
+    onChange?: ChangeEventHandler<HTMLInputElement>
 }
-const Input = (props: Props) =>{
-    return (
-    <div
-        className= {'text-field ${props}'}>
-        <label>{props.label}</label>
-        <input type={props.type} placeholder={props.label}/>
-        <span>{props.helperText}</span>
+export const TextFieldExplained = ({ 
+    name, 
+    type = "text", 
+    variant =  "one", 
+    label, 
+    helperText, 
+    onChange, 
+    errorText
+}: Props) => {
+    const [inputValue, setInputValue] = useState("");
+    const inputId = name;
+    // const emptyInput = inputValue !== "" ? "not-empty" : "";
+    let emptyInput;
+    if(inputValue == ""){
+        emptyInput = "";
+    }else{
+        emptyInput = "not-empty";
+    }
     
+    return <div className={`txt-field ${variant} ${emptyInput}`} >
+        
+        <input id={inputId} name={name} type={type} onChange={v=>{
+            setInputValue(v.target.value);
+            if(onChange) {
+                onChange(v);
+            }
+        }} />
+        <label htmlFor={inputId}>{label}</label>
+        {
+        !helperText ?  null :
+        <span className="helper-text">{helperText}</span>
+        }
+         {
+        !errorText ?  null :
+        <span className="error-text">{errorText}</span>
+        }
+        
     </div>
-    )
 }
 
-export default Input;
- 
