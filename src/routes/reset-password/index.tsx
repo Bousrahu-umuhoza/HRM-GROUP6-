46c2e8554c6253da0322dff48c9e0ee23277c8aa
Gikwayalily <gikwayaliliane@gmail.com>
2023-01-27 00:17:53 -0800

    work on new password form called reset-password

--- src/routes/reset-password/index.tsx
+++ src/routes/reset-password/index.tsx
@@ -1,7 +1,20 @@
+import { Link } from "react-router-dom"
 import { Container } from "../../components/container"
+import { TextFieldExplained } from "../../components/input"
 
 export const ResetPassword = ()=>{
     return <Container>
-        <h1>form to reset passwod</h1>
+         <div className="forgetpassword-container">
+            <h3 className="head-forgot">New Password</h3>
+            <div>
+            <TextFieldExplained  type="password" name="password" label="Enter New Password" className="input-changes" onChange={() => { } } variant="three" errorText="Error ..." placeholder={""} />
+            <br/>
+            <TextFieldExplained  type="password" name="confirm password" label="Confirm-Password" className="input-changes" onChange={() => { } } variant="three" errorText="Error ..." placeholder={""} />           
+            </div>
+            
+            <div>
+            <Link to="/dashboard"><button className="code">Send</button></Link>
+            </div>
+        </div>
     </Container>
 }
